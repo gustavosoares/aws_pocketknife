34720e25f906f072867d5a6b7b7bc3a1702cf8ba
Gustavo Soares Souza <gustavo.soares@myob.com>
2016-05-02 18:44:05 +1200

    listing all asgs

--- lib/aws_pocketknife/asg.rb
+++ lib/aws_pocketknife/asg.rb
@@ -15,8 +15,8 @@ module AwsPocketknife
         asgs = []
         asg_list = name.split(";")
         resp = @asg_client.describe_auto_scaling_groups({
-                                                         auto_scaling_group_names: asg_list,
-                                               })
+                                         auto_scaling_group_names: asg_list,
+                               })
         resp.auto_scaling_groups.each do |asg|
           asgs << asg
         end
@@ -26,15 +26,35 @@ module AwsPocketknife
       def list(max_records: 100)
         asgs = []
         resp = @asg_client.describe_auto_scaling_groups({
-                                                            max_records: max_records,
-                                                        })
-        resp.auto_scaling_groups.each do |asg|
-          asgs << asg
+                                max_records: max_records,
+                            })
+        # resp.auto_scaling_groups.each do |asg|
+        #   asgs << asg
+        # end
+        asgs << resp.auto_scaling_groups
+        next_token = resp.next_token
+        while true
+          break if next_token.nil? or next_token.length == 0
+          resp = get_asgs(next_token: next_token, max_records: max_records)
+          asgs << resp.auto_scaling_groups
+          next_token = resp.next_token
+
         end
-        asgs
+
+        asgs.flatten!
+
       end
 
       private
+
+      def get_asgs(next_token: "", max_records: 100)
+
+        resp = @asg_client.describe_auto_scaling_groups({
+                                max_records: max_records,
+                                next_token: next_token,
+                            })
+
+      end
 
     end
 
